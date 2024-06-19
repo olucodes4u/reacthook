a5505d9f692ff2c386e7684ab9a990b88c7bca9b
elonpetcode <projects@elonpetconsulting.com.ng>
2024-06-19 15:25:44 +0100

    isAuthenticated function finally implemented

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
+import { AuthContext } from '../contexts/AuthContext';
 
 class Navbar extends Component {
 
@@ -9,12 +10,17 @@ class Navbar extends Component {
       
       
         return (
-            <ThemeContext.Consumer>{(context) => {
-                const { isLightTheme, light, dark } = context;
+            <AuthContext.Consumer>{(authContext) => (
+                     <ThemeContext.Consumer>{(themeContext) => {
+                        const { isAuthenticated, toggleAuth } = authContext;
+                const { isLightTheme, light, dark } = themeContext;
                 const theme = isLightTheme ? light : dark;
                  return(
                     <nav style={{ background: theme.ui, color: theme.syntax }}>
                     <h1>Context App</h1>
+                    <div onClick={toggleAuth}>
+                        {isAuthenticated ? 'Logged in' : 'Logged out' }
+                    </div>
                     <ul>
                         <li>Home</li>
                         <li>About </li>
@@ -24,6 +30,9 @@ class Navbar extends Component {
                 </nav>
                  )
             }}</ThemeContext.Consumer>
+            )}
+         
+            </AuthContext.Consumer>
         );
     }
 }
