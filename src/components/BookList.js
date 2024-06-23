75982ed6595ec2952d0cca2773e317a638bedc71
elonpetcode <projects@elonpetconsulting.com.ng>
2024-06-23 06:24:17 +0100

    implemented useContext

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,22 +1,38 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 
-class BookList extends Component {
-    static contextType = ThemeContext;
+// class BookList extends Component {
+//     static contextType = ThemeContext;
    
-    render() { 
-        const { isLightTheme, light, dark } = this.context;
-        const theme = isLightTheme ? light : dark;
-        return (
-            <div className='book-list' style={{ color: theme.syntax, background: theme.bg }}>
-                <ul>
-                    <li style={{background: theme.ui}}>the way of king</li>
-                    <li style={{background: theme.ui}}>the way of the wind</li>
-                    <li style={{background: theme.ui}}>new man in here</li>
-                </ul>
-            </div>
-        );
-    }  
+//     render() { 
+//         const { isLightTheme, light, dark } = this.context;
+//         const theme = isLightTheme ? light : dark;
+//         return (
+//             <div className='book-list' style={{ color: theme.syntax, background: theme.bg }}>
+//                 <ul>
+//                     <li style={{background: theme.ui}}>the way of king</li>
+//                     <li style={{background: theme.ui}}>the way of the wind</li>
+//                     <li style={{background: theme.ui}}>new man in here</li>
+//                 </ul>
+//             </div>
+//         );
+//     }  
+// }
+
+const  BookList = () => {
+    const { isLightTheme, light, dark } = useContext(ThemeContext);
+    const theme = isLightTheme ? light : dark;
+    return ( 
+        <div className='book-list' style={{ color: theme.syntax, background: theme.bg }}>
+        <ul>
+            <li style={{background: theme.ui}}>the way of king</li>
+            <li style={{background: theme.ui}}>the way of the wind</li>
+            <li style={{background: theme.ui}}>new man in here</li>
+        </ul>
+    </div>
+     );
 }
+ 
+
  
 export default BookList;
--- src/components/ThemeToggle.js
+++ src/components/ThemeToggle.js
@@ -1,15 +1,13 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 
 
-class ThemeToggle extends Component {
-   static contextType = ThemeContext;
-    render() { 
-        const { toggleTheme } = this.context;
-        return (
-            <button onClick={toggleTheme}>Toggle Theme Color</button>
-        );
-    }
+ 
+const ThemeToggle = () => {
+    const { toggleTheme } =  useContext(ThemeContext);
+    return ( 
+        <button onClick={toggleTheme}>Toggle Theme Color</button>
+     );
 }
  
 export default ThemeToggle;
